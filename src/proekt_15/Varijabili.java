93c2c98ac1924a804e33c79f110a3a6f27f2391f
iki55 <aleksovska.isidora@gmail.com>
2023-11-27 21:08:21 +0100

    Add first java program

---
+++ src/proekt_15/Varijabili.java
@@ -0,0 +1,25 @@
+package proekt_15;
+import java.util.Scanner;
+public class Varijabili {
+
+	public static void main(String[] args) {
+		int x,y,z;
+	if (x>y || z>y) 
+		System.out.println("1");
+	else
+		System.out.println("2");
+	
+	if ( x>y )
+		System.out.println("3");
+	else 
+		System.out.println("4");
+	  else
+		System.out.println("5");
+	
+	
+		
+
+	}
+
+
+}
